596ce47651a1ebaa4083d22555ed2e0c7d8c679d
Guilhaof <luisphelipe1000@gmail.com>
2024-09-07 13:42:39 -0300

    new functions added in dashboard

--- src/pages/app/dashboard/index.tsx
+++ src/pages/app/dashboard/index.tsx
@@ -1,6 +1,12 @@
 import { Helmet } from "react-helmet-async";
-import { RevenueChart } from "./revenue-chart";
+
 import { PopularProductsChart } from "./popular-product-chart";
+import { MonthReceiptCard } from "./month-receipt-card";
+import { MonthOrdersAmountCard } from "./month-orders-amount-card";
+import { DayOrdersAmountCard } from "./day-orders-amount-card";
+import { MonthCanceledOrdersAmountCard } from "./month-canceled-orders-amount-card";
+import { ReceiptChart } from "./receipt-chart";
+
 
 
 export function Dashboard() {
@@ -11,10 +17,14 @@ export function Dashboard() {
         <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
         <div className="grid grid-cols-4 gap-4">
+          <MonthReceiptCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
         </div>
 
         <div className="grid grid-cols-9 gap-4">
-            <RevenueChart />
+            <ReceiptChart />
             <PopularProductsChart />
         </div>
       </div>
